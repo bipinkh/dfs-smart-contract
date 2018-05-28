b22ddd146cb13196df07d31b6786af7a36bad51c
bipinkh <bipinkh91@gmail.com>
2018-05-28 13:41:12 +0545

    optimized code for subnode operations

--- contracts/SubNode.sol
+++ contracts/SubNode.sol
@@ -1,48 +1,29 @@
 pragma solidity^0.4.19;
 import "./IsPostMan.sol";
+
 contract SubNode is IsPostMan{
 
 	struct subNodeInfo{
-	    address subNodeAddress;
+        // detail info can be stored here
+        bool isActive;
 	}
-	mapping(address=>subNodeInfo[]) subNodeMaps;
-	
+    
+	mapping(address => mapping(address => subNodeInfo) ) subNodeMaps;
+	// here the structure is ::  postman_address =>  ( subnode_address => subnodeinfo)
 	
 	function addSubNode(address postManAddress,address subNodeAddress) public onlyPostMan(postManAddress) {
-		subNodeMaps[postManAddress].push(subNodeInfo(subNodeAddress));
+		subNodeInfo memory sni = subNodeInfo(true);
+		subNodeMaps[postManAddress][subNodeAddress] = sni;
 	}
+	
+	function checkSubNodeStatus(address p, address sn) public view onlyPostMan(p) returns (bool){
+	    return subNodeMaps[p][sn].isActive;
+	}
+	
 	
 	function deleteSubNode(address postManAddress,address subNodeAddress)public onlyPostMan(postManAddress){
-	subNodeInfo[] memory totalSubNodes=subNodeMaps[postManAddress];
-	uint totalSubNodeLength=totalSubNodes.length;
+	    delete subNodeMaps[postManAddress][subNodeAddress];
+	}
 	
-		for(uint i=0;i<totalSubNodeLength;i++){
-			if(totalSubNodes[i].subNodeAddress==subNodeAddress){
-				//there is client in subscriber list
-				delete subNodeMaps[postManAddress][i];	
-			}
-		}
-	}
-	function checkSubNodeStatus(address postManAddress,address subNodeAddress)public view onlyPostMan(postManAddress) returns(bool){
-		subNodeInfo[] memory totalSubNodes=subNodeMaps[postManAddress];
-		uint totalSubNodesLength=totalSubNodes.length;
-		for(uint i=0;i<totalSubNodesLength;i++){
-			if(totalSubNodes[i].subNodeAddress==subNodeAddress){
-				//there is subnode in postman list
-				return true;
-			}
-		}
-		return false;
-	}
-	function getSubNodeList(address postManAddress)public view onlyPostMan(postManAddress)returns(address[]){
-	  uint lengthOfSubNode=subNodeMaps[postManAddress].length;
-        address[]    memory cAddress = new address[](lengthOfSubNode);
-        
-        for (uint i = 0; i < lengthOfSubNode; i++) {
-            subNodeInfo memory clientsinfo = subNodeMaps[postManAddress][i];
-            cAddress[i] = clientsinfo.subNodeAddress;
-        }
-        return cAddress;
-	
-	}
+
 }
